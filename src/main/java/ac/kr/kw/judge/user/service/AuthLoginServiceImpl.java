fc022e0f048d0d9af48c9dfd4dc0f3ec3335d6e9
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-03-14 16:52:50 +0900

    feat:login 서비스 구현

--- src/main/java/ac/kr/kw/judge/user/service/AuthLoginServiceImpl.java
+++ src/main/java/ac/kr/kw/judge/user/service/AuthLoginServiceImpl.java
@@ -1,9 +1,13 @@
 package ac.kr.kw.judge.user.service;
 
+import ac.kr.kw.judge.commons.auth.JwtUtils;
+import ac.kr.kw.judge.commons.exception.UnAuthorizedException;
+import ac.kr.kw.judge.user.domain.User;
 import ac.kr.kw.judge.user.repository.UserRepository;
 import ac.kr.kw.judge.user.service.command.AuthLoginCommand;
 import ac.kr.kw.judge.user.service.port.in.AuthLoginService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,11 +15,19 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class AuthLoginServiceImpl implements AuthLoginService {
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtils jwtUtils;
 
     @Override
     @Transactional(readOnly = true)
     public String login(AuthLoginCommand command) {
-        userRepository.findById(command)
-        return null;
+        User user = userRepository.findByUsername(command.getUsername())
+                .orElseThrow(() -> {
+                    throw new UnAuthorizedException("해당 username의 사용자는 존재하지 않습니다.");
+                });
+        if (!passwordEncoder.encode(command.getPassword()).equals(user.getPassword())) {
+            throw new UnAuthorizedException("password가 잘못됬습니다.");
+        }
+        return jwtUtils.createTokenWithUsername(user.getUsername());
     }
 }
