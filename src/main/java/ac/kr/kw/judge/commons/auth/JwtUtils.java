34349c5942a4eb60e7dec947ac022ac3c840876e
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-03-14 16:43:40 +0900

    feat:jwt token 생성 구현

---
+++ src/main/java/ac/kr/kw/judge/commons/auth/JwtUtils.java
@@ -0,0 +1,34 @@
+package ac.kr.kw.judge.commons.auth;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtils {
+    @Value("${jwt.secret}")
+    private final String secret;
+
+    @Value("${jwt.issuer}")
+    private final String issuer;
+
+    @Value("${jwt.duration_second}")
+    private final long durationSecond;
+
+    public String createTokenWithUsername(String username) {
+        Algorithm algorithm = Algorithm.HMAC512(secret);
+        LocalDateTime expiredAt = LocalDateTime.now().plusSeconds(durationSecond);
+
+        return JWT.create()
+                .withExpiresAt(Timestamp.valueOf(expiredAt))
+                .withIssuer(issuer)
+                .withClaim("username", username)
+                .sign(algorithm);
+    }
+}
