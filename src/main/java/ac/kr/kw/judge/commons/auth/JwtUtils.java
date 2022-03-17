faf8c63eda84e4f227e44fc79693d00db9594e23
CodeCommit-at-308987748304 <annyousung@gmail.com>
2022-03-17 12:46:34 +0900

    fix:JwtUtils code 정리

--- src/main/java/ac/kr/kw/judge/commons/auth/JwtUtils.java
+++ src/main/java/ac/kr/kw/judge/commons/auth/JwtUtils.java
@@ -2,7 +2,6 @@ package ac.kr.kw.judge.commons.auth;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
