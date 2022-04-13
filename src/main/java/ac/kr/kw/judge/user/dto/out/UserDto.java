57687ce70aebcf67d7489d60ec345a68787b1e80
bugdebugging <annyousung@gmail.com>
2022-04-13 21:43:47 +0900

    fix:userDto rank 추가

--- src/main/java/ac/kr/kw/judge/user/dto/out/UserDto.java
+++ src/main/java/ac/kr/kw/judge/user/dto/out/UserDto.java
@@ -6,14 +6,16 @@ public class UserDto {
     private Long id;
     private String name;
     private int accumulateScore;
+    private String rank;
 
-    private UserDto(Long id, String name, int accumulateScore) {
+    private UserDto(Long id, String name, int accumulateScore, String rank) {
         this.id = id;
         this.name = name;
         this.accumulateScore = accumulateScore;
+        this.rank = rank;
     }
 
     public static UserDto fromEntity(User user) {
-        return new UserDto(user.getId(), user.getUsername(), user.getAccumulateScore());
+        return new UserDto(user.getId(), user.getUsername(), user.getAccumulateScore(), user.getRank().getValue());
     }
 }
